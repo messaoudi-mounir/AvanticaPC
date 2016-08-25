58888b0074c598d36238d72ca0c5868129ef2921
Alejandro Quesada <aquesada@connecture.com>
2016-08-25 01:13:47 -0600

    Revert "no message"
    This reverts commit c4e5e2edde564d0269d26969b16e31aceebe7d8f.

--- Automation training/AutomationTraining/src/Stage1/LogInTests.java
+++ Automation training/AutomationTraining/src/Stage1/LogInTests.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -30,17 +28,14 @@ public class LogInTests {
 	//Setting up the firefox driver and URL to work with.
 	@BeforeMethod(alwaysRun = true)
 	public void setUp() throws Exception {
-		System.out.println("Creating driver...");
 		driver = new FirefoxDriver();
-	    baseUrl = "http://192.168.0.103:86/";	    
+	    baseUrl = "http://192.168.0.103:86/";
 	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	    
 	}
 	@BeforeTest
 	public void startExecution (){
 		System.out.println("Starting Execution...");
 	}
-	
-	//Shuts down the Firefox driver
 	
 	   /* Excecutes the test, follows the workflow
 	    * 1-Adding the Login path to t he baseUrl
@@ -71,7 +66,6 @@ public class LogInTests {
 	    driver.findElement(By.id("ctl00_Main_LoginConrol_Password")).sendKeys("Ljnd1709#");
 	    driver.findElement(By.id("ctl00_Main_LoginConrol_LoginButton")).click();
 	    assertEquals(driver.findElement(By.id("ctl00_LoginView_MemberLoginStatus")).getText(), "Logout");
-	    assertTrue(driver.findElement(By.id("ctl00_Main_LoginConrol_UserName")).getText() == "aquesada");
 	  } 
 	  /* Excecutes the test, follows the workflow
 	   * 1-Adding the Login path to the baseUrl
@@ -89,25 +83,14 @@ public class LogInTests {
 	    driver.findElement(By.id("ctl00_Main_LoginConrol_Password")).sendKeys("password");
 	    driver.findElement(By.id("ctl00_Main_LoginConrol_LoginButton")).click();
 	    assertEquals(driver.findElement(By.xpath("//table[@id='ctl00_Main_LoginConrol']/tbody/tr/td/table/tbody/tr[4]/td")).getText(), "Your login attempt was not successful. Please try again.");
-	  }  
-	  
-	  @AfterMethod(alwaysRun = true)
+	  }
+	  //Shuts down the Firefox driver
+	  @AfterClass(alwaysRun = true)
 	  public void tearDown() throws Exception {
-		  System.out.println("Deleating driver...");
-		  driver.quit();
-		  String verificationErrorString = verificationErrors.toString();
-		  if (!"".equals(verificationErrorString)) {
-			  fail(verificationErrorString);
-		  }
-	  }
-	  
-	  @AfterTest
-	  public void afterTestMethod(){
-		  System.out.println("Completing Execution...");
-	  }
-	  
-	  @AfterClass
-	  public void afterClassMethod(){
-		  System.out.println("Completing execution class: LogInTests");
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
 	  }
 }
--- Automation training/AutomationTraining/src/Stage1/RegisterTests.java
+++ Automation training/AutomationTraining/src/Stage1/RegisterTests.java
@@ -1,8 +1,6 @@
 package Stage1;
 
 
-import static org.testng.Assert.fail;
-
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -11,9 +9,6 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,12 +16,11 @@ import org.testng.annotations.Test;
 public class RegisterTests{
 	private WebDriver driver;
 	private String baseUrl;
-	private StringBuffer verificationErrors = new StringBuffer();
 
 	
 	@BeforeClass
 	public void className (){
-		System.out.println("Executing class: RegisterTests");
+		System.out.println("Executing class: LogInTests");
 	}
 	//Setting up the firefox driver and URL to work with.
 	@BeforeMethod(alwaysRun = true)
@@ -86,26 +80,6 @@ public class RegisterTests{
 		
 		verifyPassAndConfirm();
 	}
-	
-	 @AfterMethod(alwaysRun = true)
-	  public void tearDown() throws Exception {
-		  System.out.println("Deleating driver...");
-		  driver.quit();
-		  String verificationErrorString = verificationErrors.toString();
-		  if (!"".equals(verificationErrorString)) {
-			  fail(verificationErrorString);
-		  }
-	  }
-	  
-	  @AfterTest
-	  public void afterTestMethod(){
-		  System.out.println("Completing Execution...");
-	  }
-	  
-	  @AfterClass
-	  public void afterClassMethod(){
-		  System.out.println("Completing execution class: RegisterTests");
-	  }
 	
 	public void verifyPassAndConfirm(){
 		Assert.assertTrue(driver.findElement(By.id("ctl00_Main_CreateUserWizardControl_CreateUserStepContainer_PasswordCompare")).isDisplayed());
